e05b44e60308542b69985a3adbf20ca7cd5dd984
JuCas <julien862@hotmail.com>
2020-09-30 19:36:36 +0200

    first

---
+++ firstApp/src/firstApp/mainClass.java
@@ -0,0 +1,17 @@
+package firstApp;
+
+import java.util.Scanner;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// comment
+ Scanner input = new Scanner(System.in) ;
+	String test = input.toString();
+	System.out.println(test);
+	input.close();
+}
+
+	}
+
+
